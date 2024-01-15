7e691bb0dc9dbf7fcfb463a96817ffb24b94b2f4
Lars Reuter <lp.reuter@gmail.com>
2024-01-15 20:49:05 +0100

    Add hue selector

--- src/App.js
+++ src/App.js
@@ -2,27 +2,8 @@ import './App.css';
 import './style.css';
 import randomColor from 'randomcolor'; // import random color package
 import React, { useState } from 'react';
+import RandomColorWidget from './RandomColorWidget';
 
 export default function App() {
-  const initialColor = randomColor();
-  const [color, setColor] = useState(initialColor);
-  return (
-    <div className="container">
-      <div className="div">
-        <div className="widget" style={{ backgroundColor: color }}>
-          Generated Color: {color}
-        </div>
-        <button
-          className="button"
-          onClick={() => {
-            const newColor = randomColor();
-            setColor(newColor);
-          }}
-        >
-          Generate
-        </button>
-        <div className="text-wrapper-3">Random Color Generator Widget</div>
-      </div>
-    </div>
-  );
+  return <RandomColorWidget />;
 }
